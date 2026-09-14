435019a094be00cdde3b0a25219c73e9c89090fb
VladTachutdinov <vladiktach1999@gmail.com>
2026-09-14 19:54:58 +0500

    Task3

---
+++ Tyuiu.TachutdinovVI.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.TachutdinovVI.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.TachutdinovVI.Sprint0.Task3.V0.Test;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void CheckedValid()
+    {
+        Assert.AreEqual(10, DataService.Sum(5,5));
+    }
+}
