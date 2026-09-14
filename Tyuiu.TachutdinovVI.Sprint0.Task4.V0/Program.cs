7865a55965ed33dc776d81c57e680c0014c46772
VladTachutdinov <vladiktach1999@gmail.com>
2026-09-14 20:51:11 +0500

    Task5

--- Tyuiu.TachutdinovVI.Sprint0.Task4.V0/Program.cs
+++ Tyuiu.TachutdinovVI.Sprint0.Task4.V0/Program.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.TachutdinovVI.Sprint0.Task4.V0
             Console.WriteLine(DataService.Subtraction(15, 5));
             Console.WriteLine(DataService.Multiplication(10,10));
             Console.WriteLine(DataService.Division(5,5));
-            Console.ReadKey()
+            Console.ReadKey();
         }
     }
     }    
