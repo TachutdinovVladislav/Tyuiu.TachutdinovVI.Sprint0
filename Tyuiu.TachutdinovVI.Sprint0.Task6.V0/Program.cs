d402e0ef37b6fe4638ec6b730952709a251650ab
VladTachutdinov <vladiktach1999@gmail.com>
2026-09-15 20:38:07 +0500

    Task6

---
+++ Tyuiu.TachutdinovVI.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.TachutdinovVI.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.TachutdinovVI.Sprint0.Task6.V0;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
+        Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(arraynums));
+        Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+        Console.ReadKey();
+    }
+}
+
